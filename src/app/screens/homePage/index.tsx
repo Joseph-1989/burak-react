bc432d75c2eb812b6cb035749d55da1fbe727819
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-04-21 12:16:28 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,12 +7,29 @@ import Events from "./Events";
 import "../../../css/home.css";
 import { useEffect } from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+
+/** REDUX SLICE & SELECTOR */
+
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
+
 export default function HomePage() {
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { popularDishes } = useSelector(popularDishesRetriever);
   // Selector: Store => Data
-  useEffect(() => {
-    // Backend server data request => Data
-    // Slice: Data => Store
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className={"homepage"}>
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,7 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import HomePageReducer from "./screens/homePage/slice";
 import reduxLogger from "redux-logger";
+
 export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     // @ts-ignore
